c0f922589b6fa6f73080c0a0e1893564e606ea28
MateenKhan <makjavaprogrammer@gmail.com>
2022-05-09 11:33:11 +0530

    1 file modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContentNavComponent } from './content-nav/content-nav.component';
+import { ContentNavComponent } from './overview/content-nav.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { Injector } from '@angular/core';
